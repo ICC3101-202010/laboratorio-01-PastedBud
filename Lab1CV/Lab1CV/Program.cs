5ca47d0adb8d3c9d0037cf78e9585337cab514e2
Carlo Vitali <Carlo@Air-de-Carlos.iptv.microsoft.com>
2020-03-24 16:12:03 -0300

    parte1

--- Lab1CV/Lab1CV/Program.cs
+++ Lab1CV/Lab1CV/Program.cs
@@ -2,12 +2,34 @@
 
 namespace Lab1CV
 {
-    class MainClass
+    class Person
     {
-        public static void Main(string[] args)
+        private string Name;
+        private string Lastname;
+
+
+        public Person(string name, string lastname)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Segundo write");
+            this.Name = name;
+            this.Lastname = lastname;
         }
+
+        public int lanzar()
+        {   Random rnd = new Random();
+            int numero = rnd.Next(0, 3);
+            return numero;
+        }
+            
+     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Bob", "Kunga");
+            Console.WriteLine(p1.lanzar());
+         }
+
+
     }
 }
